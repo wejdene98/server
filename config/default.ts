9f46d7f066bdda4eaa972025d8725280a5425f20
wejdene bedoui <wejdene.bedoui98@gmail.com>
2022-09-13 23:34:53 +0100

    deploy vercel

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,6 @@
 
 export default {
     port:4000,
-    dbUri: "mongodb://localhost:27017/url-shortener",
-  }; 
+  dbUri: "mongodb://localhost:27017/url-shortener",
+  corsOrigin: "http://localhost:3000",
+}; 
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,16 @@ import config from 'config';
 import routes from "./routes";
 import bodyParser from 'body-parser';
 import db from './db';
-
+import cors from 'cors';
 
 const app = express(); 
+app.use(cors({
+    origin: config.get('corsOrigin')
+})
+);
+
+
+
 const port = config.get("port");
 
 app.use(bodyParser.json());
